b4c46c47238aa67e6f64fdefaba440cad2c152ce
Jason <Jason@DESKTOP-RUV11KO>
2016-04-05 10:10:48 +0800

    Change Windows Setting

--- src/com/team/ap/APActivity.java
+++ src/com/team/ap/APActivity.java
@@ -2,12 +2,16 @@ package com.team.ap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class APActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// 初期加载时不显示title
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
     }
 }

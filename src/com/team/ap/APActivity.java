675703db11baaa1399441f52ce6e21d2d3d2c755
Jason <Jason@DESKTOP-RUV11KO>
2016-04-11 10:37:19 +0800

    Signed-off-by: Jason <Jason@DESKTOP-RUV11KO>

--- src/com/team/ap/APActivity.java
+++ src/com/team/ap/APActivity.java
@@ -3,14 +3,14 @@ package com.team.ap;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-
 public class APActivity extends Activity {
+	//private UMSocialService mController = UMServiceFactory.getUMSocialService("com.open.share");
     /** Called when the activity is first created. */
     @Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 		// 初期加载时不显示title
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
     }
 }
